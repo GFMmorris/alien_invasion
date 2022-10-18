f482309a11544aceafc3d5e176db3420da97dbaf
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-10-18 09:49:50 -0400

    Ship file creation
    The ship file has been created and now starts the ship at the bottom of the screen every time.

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Init game settings
         # Screnn Settings"""
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 600
         self.bg_color = (166, 166, 166)
