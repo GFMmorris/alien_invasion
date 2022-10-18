928af5e3b4b649e34c85c2a01d255586a1f32e8e
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-10-18 09:07:36 -0400

    Added Settings file to project
    Settings file created to modify and maintain all initial settings for game

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,28 +4,42 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
+
     def __init__(self):
         """Initialize the game, and create game resources."""
+        # (1)
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        # (2)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Start the main loop for the game."""
+        # (3)
         while True:
-            #Watch the keyboard and mouse events.
+            # Watch the keyboard and mouse events.
+            # (4)
             for event in pygame.event.get():
+                # (5)
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-                #Make the most recently drawn screen visible.
+                # Redraw the screen during each pass through the loop.
+                self.screen.fill(self.settings.bg_color)
+
+                # Make the most recently drawn screen visible.
+                # (6)
                 pygame.display.flip()
 
+
 if __name__ == '__main__':
-    #make a game instance and run the game.
+    # make a game instance and run the game.
     ai = AlienInvasion()
     ai.run_game()
-
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for Alie Invasion"""
+
+    def __init__(self):
+        """Init game settings
+        # Screnn Settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (166, 166, 166)
