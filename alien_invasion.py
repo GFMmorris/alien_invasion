dd8fcf9575aa6e8488d44a05e8a2cb4691813d39
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-10-25 08:34:03 -0400

    Added left s direction movement
    Left and right now work the same way with neither taking priority

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,9 +43,15 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     # move the ship to the right
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # move the ship to the left
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    # stopping move the ship to the left
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on te screen, and flip to the new screen."""
