dba7a679523813562012d5b45a7141a1cf0578b2
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-10-25 09:36:31 -0400

    Added the Bullet.py file
    Adding the file and making it possible to draw a bullet on the ships position and sending the bullet off the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -24,6 +25,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -33,6 +35,7 @@ class AlienInvasion:
             # (4)
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -56,6 +59,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """respond to key releases"""
@@ -65,11 +70,17 @@ class AlienInvasion:
             # stopping move the ship to the left
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on te screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
