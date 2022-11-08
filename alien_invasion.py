eca54b63c939e67bd85e074e7bfdec8910a74f35
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-11-08 08:58:40 -0500

    Game end conditions
    Now the game will end if the fleet hits the bottom of the screen. Or if the ship collides with an alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -136,6 +136,10 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # make aliens
@@ -164,11 +168,22 @@ class AlienInvasion:
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+
+
     def _check_fleet_edges(self):
         """Respond appropriately should you bump into a wall with a spaceship-"""
         for alien in self.aliens.sprites():
             if alien.check_edges():
                 self._change_fleet_direction()
+                break
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat theis the same as if the ship got hit.
+                self._ship_hit()
                 break
 
     def _change_fleet_direction(self):
