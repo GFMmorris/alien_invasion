01555e49c6b4a81fa96b15f4e838210ae6a213ad
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-11-13 15:05:03 -0500

    updates

---
+++ homework2/13-5/game_stats.py
@@ -0,0 +1,10 @@
+class GameStats:
+
+    def __init__(self, ai_game):
+        self.ship_limit = 3
+        self.reset_stats()
+
+        self.ggame_active = True
+
+    def reset_stats(self):
+        self.ships_left = self.ship_limit
